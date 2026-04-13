f5c8aee8fc8afa5f9676104cb957078fddf7cb1b
Burçin CANDAN <github@burcincandan.com>
2026-04-13 23:32:37 +0300

    Fix local dev routing

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,25 @@
 import { defineConfig } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
-export default defineConfig({
-  base: '/ataortaokulu-sinav/',
-  plugins: [viteSingleFile()],
-  server: {
-    port: 5173,
-    strictPort: false,
-  },
-  publicDir: 'public',
-  build: {
-    target: 'esnext',
-    outDir: 'dist',
-    assetsInlineLimit: 100000000,
-    chunkSizeWarningLimit: 100000000,
-    cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      input: 'index.html'
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/ataortaokulu-sinav/' : './',
+    plugins: [viteSingleFile()],
+    server: {
+      port: 5173,
+      strictPort: false,
     },
-  },
+    publicDir: 'public',
+    build: {
+      target: 'esnext',
+      outDir: 'dist',
+      assetsInlineLimit: 100000000,
+      chunkSizeWarningLimit: 100000000,
+      cssCodeSplit: false,
+      brotliSize: false,
+      rollupOptions: {
+        input: 'index.html'
+      },
+    },
+  };
 });
